45758cfd142280fc5933775ee7b6ce606bbbd0e9
Cameron <2974614+cameronf87@users.noreply.github.com>
2021-03-18 12:35:47 -0500

    Delete key/value pair

---
+++ delete-pair.js
@@ -0,0 +1,15 @@
+var request = new XMLHttpRequest();
+
+request.open('DELETE', 'https://dictionary.iachieved.it/dictionary/id/keys/key');
+
+request.setRequestHeader('Content-Type', 'application/json');
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
+};
+
+request.send();
